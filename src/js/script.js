d8eee9eb2ac0ee97732c954638d700ddb5e6304f
Dima <gutenko.dim@yandex.ru>
2021-07-17 23:36:28 +0500

    modified block portfolio, connected phpmailer, fixed bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,71 @@
-const hamburger = document.querySelector('.hamburger'),
+window.addEventListener('DOMContentLoaded', () => {
+
+    const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       menuLink = document.querySelectorAll('.menu__link'),
       closeElem = document.querySelector('.menu__close'),
       overlay = document.querySelector('.menu__overlay'),
-      anchors = document.querySelectorAll('a[href*="#"]')
+      anchors = document.querySelectorAll('a[href*="#"]'),
+      form = document.querySelector('.contacts__form')
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-window.addEventListener('click', (e) => {
-    if (e.target == overlay) {
+    closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
-    }
-});
+    });
 
-menuLink.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        menu.classList.remove('active');
-    })
-})
+    window.addEventListener('click', (e) => {
+        if (e.target == overlay) {
+            menu.classList.remove('active');
+        }
+    });
 
-anchors.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-        const blockID = item.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+    menuLink.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            menu.classList.remove('active');
         })
     })
-})
 
-const percents = document.querySelectorAll('.progress__item-percent'),
-      lines = document.querySelectorAll('.progress__item-line span');
+    anchors.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const blockID = item.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    })
+
+    const percents = document.querySelectorAll('.progress__item-percent'),
+        lines = document.querySelectorAll('.progress__item-line span');
 
 
-percents.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
+    percents.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
+
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(form)
+
+        fetch('mailer/smart.php', {
+            method: 'POST',
+            body: formData,
+        })
+        .then (() => {
+            alert('Запрос отправлен');
+        })
+        .catch(() => {
+            alert('Произошел сбой при отправке данных');
+        })
+        .finally(() => {
+            form.reset();
+        })
+    });
+    
 });
